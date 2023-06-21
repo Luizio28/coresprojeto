0f9405da74114c64a8144ca9d37a44e148f8de61
Kaique Nobre <dankmemerson@tutanota.com>
2023-06-21 13:46:43 -0300

    made sql_connector DRYer

--- _scripts/sql_db_connector.php
+++ _scripts/sql_db_connector.php
@@ -1,43 +1,32 @@
 <?php
-
-//high DRYification potential
-
-function connect_to_db()
+function get_database_config()
 {
     $server_ip = $_SERVER['SERVER_ADDR'];
     $is_local = ($server_ip === '127.0.0.1' || $server_ip === '::1');
 
-    if ($is_local) {
-        $db_hostname = 'localhost';
-        $db_user = 'root';
-        $db_pass = '';
-        $db_name = 'requerimentos';
-    } else { //nn vou vazar dado de server nem a pau
-        $db_hostname = 'remote_server_ip';
-        $db_user = 'remote_username';
-        $db_pass = 'remote_password';
-        $db_name = 'remote_database';
-    }
+    return $is_local ?
+        [
+            'db_hostname' => 'localhost',
+            'db_user' => 'root',
+            'db_pass' => '',
+            'db_name' => 'requerimentos',
+        ] : [
+            'db_hostname' => 'remote_server_ip',
+            'db_user' => 'remote_username',
+            'db_pass' => 'remote_password',
+            'db_name' => 'remote_database',
+        ];
+}
 
-    return new mysqli($db_hostname, $db_user, $db_pass, $db_name);
+function connect_to_db()
+{
+    $config = get_database_config();
+    return new mysqli($config['db_hostname'], $config['db_user'], $config['db_pass'], $config['db_name']);
 }
 
 function connect_with_pdo()
 {
-    $server_ip = $_SERVER['SERVER_ADDR'];
-    $is_local = ($server_ip === '127.0.0.1' || $server_ip === '::1');
-
-    if ($is_local) {
-        $db_hostname = 'localhost';
-        $db_user = 'root';
-        $db_pass = '';
-        $db_name = 'requerimentos';
-    } else { //nn vou vazar dado de server nem a pau
-        $db_hostname = 'remote_server_ip';
-        $db_user = 'remote_username';
-        $db_pass = 'remote_password';
-        $db_name = 'remote_database';
-    }
-
-    return new PDO("mysql:host=$db_hostname;dbname=$db_name", $db_user, $db_pass);
+    $config = get_database_config();
+    $dsn = "mysql:host={$config['db_hostname']};dbname={$config['db_name']}";
+    return new PDO($dsn, $config['db_user'], $config['db_pass']);
 }
