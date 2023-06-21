41faead846cd034aeadd7b265e77858fc2c9e30f
Kaique Nobre <dankmemerson@tutanota.com>
2023-06-21 13:30:00 -0300

    Chaging connection type to PDO

--- _scripts/account_creator.php
+++ _scripts/account_creator.php
@@ -7,13 +7,25 @@ if (isset($send)) {
     try {
         $db_connection = connect_to_db();
 
+        $pdo = connect_with_pdo();
+
+        $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+        $stmt = $pdo->prepare("INSERT INTO discente (matricula, nome, email, fone, curso, turma, psswd)
+                               VALUES (:matricula, :nome, :email, :fone, :curso, :turma, :psswd");
+
+
         $hashed_psswd = hash('sha256', $psswd, true);
-        $hashed_matricula = hash('sha256', $matricula, true);
 
-        $db_consult = "INSERT INTO discente VALUES ('$hashed_matricula','$nome','$email','$fone','$curso','$turma','$hashed_psswd')";
+        $stmt->bindParam(':matricula', $matricula);
+        $stmt->bindParam(':nome', $nome);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':fone', $fone);
+        $stmt->bindParam(':curso', $curso);
+        $stmt->bindParam(':turma', $turma);
+        $stmt->bindParam(':psswd', $hashed_psswd);
 
-
-        $sql_query = mysqli_query($db_connection, $db_consult);
+        $stmt->execute();
     } catch (Throwable $th) {
         echo "
         <div class='flex-column'>
