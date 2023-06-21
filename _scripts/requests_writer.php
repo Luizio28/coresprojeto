5b4291bc1bd3fb7790c9e810f97975cb4ec844ff
Kaique Nobre <dankmemerson@tutanota.com>
2023-06-21 12:05:58 -0300

    Changed the connection type to pdo for higher security

--- _scripts/requests_writer.php
+++ _scripts/requests_writer.php
@@ -3,16 +3,32 @@ include "../_scripts/sql_db_connector.php";
 
 extract($_POST);
 
+//high DRYification potential
+
 if (isset($send)) {
     try {
-        $db_connection = connect_to_db();
+        $pdo = connect_with_pdo();
 
-        $discente_id = $_COOKIE[$cookieName];
+        $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
-        $db_consult = "INSERT INTO requerimento VALUES ('$discente_id','$objeto', '$inicio', '$termino', '$anexos', '$obs', '$situacao')";
+        $stmt = $pdo->prepare("INSERT INTO requerimento (id, discente_id, objeto, inicio, termino, registro, anexo, obs, situacao)
+                               VALUES (:id, :discente_id, :objeto, :inicio, :termino, :registro, :anexo, :obs, :situacao)");
 
-        $sql_query = mysqli_query($db_connection, $db_consult);
-    } catch (Throwable $th) {
+        $id = null;
+        $registro = null;
+
+        $stmt->bindParam(':id', $id);
+        $stmt->bindParam(':discente_id', $discente_id);
+        $stmt->bindParam(':objeto', $objeto);
+        $stmt->bindParam(':inicio', $inicio);
+        $stmt->bindParam(':termino', $termino);
+        $stmt->bindParam(':registro', $registro);
+        $stmt->bindParam(':anexo', $anexo);
+        $stmt->bindParam(':obs', $obs);
+        $stmt->bindParam(':situacao', $situacao);
+
+        $stmt->execute();
+    } catch (PDOException $e) {
         echo "
         <div class='flex-column'>
             <h1>ERRO</h1>
--- _scripts/sql_db_connector.php
+++ _scripts/sql_db_connector.php
@@ -18,3 +18,22 @@ function connect_to_db()
 
     return new mysqli($db_hostname, $db_user, $db_pass, $db_name);
 }
+
+function connect_with_pdo(){
+    $server_ip = $_SERVER['SERVER_ADDR'];
+    $is_local = ($server_ip === '127.0.0.1' || $server_ip === '::1');
+
+    if ($is_local) {
+        $db_hostname = 'localhost';
+        $db_user = 'root';
+        $db_pass = '';
+        $db_name = 'requerimentos';
+    } else { //nn vou vazar dado de server nem a pau
+        $db_hostname = 'remote_server_ip';
+        $db_user = 'remote_username';
+        $db_pass = 'remote_password';
+        $db_name = 'remote_database';
+    }
+
+    return new PDO("mysql:host=$db_hostname;dbname=$db_name", $db_user, $db_pass);
+}
