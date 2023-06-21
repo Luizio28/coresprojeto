1332e1e5aaff40e2e8f80af86ef01e4f55e501dc
Kaique Nobre <dankmemerson@tutanota.com>
2023-06-20 21:20:49 -0300

    It's theorically possible to Admins to log-in now 	modified:   _scripts/account_reader.php

--- _scripts/account_reader.php
+++ _scripts/account_reader.php
@@ -8,11 +8,11 @@ function login_attempt()
         $is_local = ($server_ip === '127.0.0.1' || $server_ip === '::1');
 
         if ($is_local) {
-
             $db_hostname = 'localhost';
             $db_user = 'root';
             $db_pass = '';
             $db_name = 'requerimentos';
+
         } else { //nn vou vazar dado de server nem a pau
             $db_hostname = 'remote_server_ip';
             $db_user = 'remote_username';
@@ -22,19 +22,41 @@ function login_attempt()
 
         $db_connection = new mysqli($db_hostname, $db_user, $db_pass, $db_name);
 
-        $result = $db_connection->query("SELECT * FROM discente");
+        switch ($nome){
+            case 7:
+                $result = $db_connection->query("SELECT * FROM administradores");
 
-        while ($row = $result->fetch_assoc()) {
-            if ($nome == $row['nome'] & $psswd == $row['psswd']) {
-                $is_in_db = true;
-            }
+                while ($row = $result->fetch_assoc()) {
+                    if ($nome == $row['nome'] & $psswd == $row['psswd']) {
+                        $is_in_db = true;
+                    }
+                }
+
+                header(($is_in_db) ? "Location: ../docentes/" : "Location: ../home/"); 
+            case 12:
+                $result = $db_connection->query("SELECT * FROM discente");
+
+                while ($row = $result->fetch_assoc()) {
+                    if ($nome == $row['nome'] & $psswd == $row['psswd']) {
+                        $is_in_db = true;
+                    }
+                }
+
+                header(($is_in_db) ? "Location: ../discentes/" : "Location: ../home/"); 
         }
-
-        header(($is_in_db) ? "Location: ../discentes/" : "Location: ../home/"); //To do, fazer redirecionamento pra aba de docentes
-
+        
         $db_connection->close();
         
     } catch (Throwable $th) {
+        echo "
+        <div class='flex-column'>
+            <h1>ERRO</h1>
+            
+            <div class='box'>
+                <p>Dados de login incorretos ou inválidos</p>
+            </div>
+        </div>
+        ";
         header("Location: ../home/");
     }
 }
