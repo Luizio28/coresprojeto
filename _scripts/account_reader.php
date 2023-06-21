fd601f82245c4b65d8c8d6d4fe913b4fa2ab62b9
Kaique Nobre <dankmemerson@tutanota.com>
2023-06-21 10:55:32 -0300

    Made code DRYer

--- _scripts/account_reader.php
+++ _scripts/account_reader.php
@@ -8,55 +8,40 @@ function login_attempt()
     try {
         $db_connection = connect_to_db();
 
-        switch ($nome){
+        $out_domain = "Location: ../home/";
+
+        switch ($nome) {
             case 7:
                 $result = $db_connection->query("SELECT * FROM administradores");
 
-                while ($row = $result->fetch_assoc()) {
-                    if ($nome == $row['nome'] & $psswd == $row['psswd']) {
-                        $is_in_db = true;
-                    }
-                }
-
-                if (!$is_in_db){
-                    echo "
-                    <div class='flex-column'>
-                        <h1>ERRO</h1>
-                        
-                        <div class='box'>
-                            <p>Dados de login incorretos ou inválidos</p>
-                        </div>
-                    </div>
-                    ";
-                }
-
-                header(($is_in_db) ? "Location: ../docentes/" : "Location: ../home/"); 
+                $out_domain = "Location: ../docentes/";
             case 12:
                 $result = $db_connection->query("SELECT * FROM discente");
 
-                while ($row = $result->fetch_assoc()) {
-                    if ($nome == $row['nome'] & $psswd == $row['psswd']) {
-                        $is_in_db = true;
-                    }
-                }
-
-                if (!$is_in_db){
-                    echo "
-                    <div class='flex-column'>
-                        <h1>ERRO</h1>
-                        
-                        <div class='box'>
-                            <p>Dados de login incorretos ou inválidos</p>
-                        </div>
-                    </div>
-                    ";
-                }
-
-                header(($is_in_db) ? "Location: ../discentes/" : "Location: ../home/"); 
+                $out_domain = "Location: ../discentes/";
         }
-        
+
+        while ($row = $result->fetch_assoc()) {
+            if ($nome == $row['nome'] & $psswd == $row['psswd']) {
+                $is_in_db = true;
+            }
+        }
+
+        if (!$is_in_db) {
+            echo "
+            <div class='flex-column'>
+                <h1>ERRO</h1>
+                
+                <div class='box'>
+                    <p>Dados de login incorretos ou inválidos</p>
+                </div>
+            </div>
+            ";
+        }
+
+        header(($is_in_db) ? $out_domain : "Location: ../home/");
+
         $db_connection->close();
-        
     } catch (Throwable $th) {
         echo "
         <div class='flex-column'>
