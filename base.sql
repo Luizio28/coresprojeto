9015d16a5433001c22fcf102a37e62192b2c6346
Kaique Nobre <dankmemerson@tutanota.com>
2023-06-22 20:13:10 -0300

    Moved the table falta down  so it doesn't generate a mysql/mariadb error upon creation

--- base.sql
+++ base.sql
@@ -25,15 +25,15 @@ CREATE TABLE requerimento (
     FOREIGN KEY (usuario_id) REFERENCES usuario(id)
 );
 
+CREATE TABLE docente (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome CHAR(255) NOT NULL
+);
+
 CREATE TABLE falta (
     faltas INT(1),
     requerimento_id INT,
     docente_id INT,
     FOREIGN KEY (requerimento_id) REFERENCES requerimento(id),
     FOREIGN KEY (docente_id) REFERENCES docente(id)
-);
-
-CREATE TABLE docente (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nome CHAR(255) NOT NULL
 );
