4ed2402baa2dcdb16935dbae06c0c17c97ae4bf2
Kaique Nobre <dankmemerson@tutanota.com>
2023-06-22 15:34:58 -0300

    Adapted the prepare function to the recent DB changes

--- _scripts/requests_writer.php
+++ _scripts/requests_writer.php
@@ -11,21 +11,24 @@ if (isset($send)) {
 
         $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
-        $stmt = $pdo->prepare("INSERT INTO requerimento (id, discente_id, objeto, inicio, termino, registro, anexo, obs, situacao)
-                               VALUES (:id, :discente_id, :objeto, :inicio, :termino, :registro, :anexo, :obs, :situacao)");
+        $stmt = $pdo->prepare("INSERT INTO requerimento (id, objeto, inicio, termino, registro, anexo, obs, situacao, discente_id)
+                               VALUES (:id, :objeto, :inicio, :termino, :registro, :anexo, :obs, :situacao, :discente_id)");
 
         $id = null;
         $registro = null;
 
         $stmt->bindParam(':id', $id, PDO::PARAM_INT);
-        $stmt->bindParam(':discente_id', $discente_id, PDO::PARAM_INT, 12);
+
         $stmt->bindParam(':objeto', $objeto, PDO::PARAM_INT, 1);
         $stmt->bindParam(':inicio', $inicio);
         $stmt->bindParam(':termino', $termino);
         $stmt->bindParam(':registro', $registro);
+
         $stmt->bindParam(':anexo', $anexo, PDO::PARAM_LOB);
         $stmt->bindParam(':obs', $obs, PDO::PARAM_STR_CHAR, 255);
         $stmt->bindParam(':situacao', $situacao, PDO::PARAM_INT, 1);
+
+        $stmt->bindParam(':discente_id', $discente_id, PDO::PARAM_INT, 12);
 
         $stmt->execute();
     } catch (PDOException $e) {
