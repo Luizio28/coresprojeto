5a16ddd3c53358a64781d61b77f313e138b3d600
Kaique Nobre <dankmemerson@tutanota.com>
2023-06-23 11:11:30 -0300

    Removed all the columns that auto increment for better readability

--- _scripts/requests_writer.php
+++ _scripts/requests_writer.php
@@ -10,22 +10,15 @@ if (isset($_POST['send'])) {
 
         $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
-        $stmt = $pdo->prepare("INSERT INTO requerimento (id, objeto, inicio, termino, registro, anexo, obs, situacao, usuario_id)
-                               VALUES (:id, :objeto, :inicio, :termino, :registro, :anexo, :obs, :situacao, :usuario_id)");
-
-        $id = null;
-        $registro = null;
-
-        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+        $stmt = $pdo->prepare("INSERT INTO requerimento (objeto, inicio, termino, anexo, obs, usuario_id)
+                               VALUES (:objeto, :inicio, :termino, :anexo, :obs, :usuario_id)");
 
         $stmt->bindParam(':objeto', $_POST['objeto'], PDO::PARAM_INT, 1);
         $stmt->bindParam(':inicio', $_POST['inicio']);
         $stmt->bindParam(':termino', $_POST['termino']);
-        $stmt->bindParam(':registro', $registro);
 
         $stmt->bindParam(':anexo', $_POST['anexo'], PDO::PARAM_LOB);
         $stmt->bindParam(':obs', $_POST['obs'], PDO::PARAM_STR_CHAR, 255);
-        $stmt->bindParam(':situacao', $_POST['situacao'], PDO::PARAM_INT, 1);
 
         $stmt->bindParam(':usuario_id', $_POST['usuario_id'], PDO::PARAM_INT, 12);
 
