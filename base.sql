4051d031c2494304dabe06de72d19376474ba262
Kaique Nobre <dankmemerson@tutanota.com>
2023-06-23 11:08:22 -0300

    made some changes to the DB for better storage efficiency

--- base.sql
+++ base.sql
@@ -6,21 +6,21 @@ CREATE TABLE usuario (
     nome CHAR(255) NOT NULL,
     email CHAR(255) NOT NULL,
     fone BIGINT(11) UNSIGNED,
-    curso INT(1) NOT NULL,
-    turma INT(1),
+    curso TINYINT(1) NOT NULL,
+    turma TINYINT(1),
     superuser BOOLEAN DEFAULT 0,
     psswd char(255) NOT NULL
 );
 
 CREATE TABLE requerimento (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    objeto INT(1) NOT NULL,
+    objeto TINYINT(1) NOT NULL,
     inicio DATE NOT NULL,
     termino DATE NOT NULL,
-    registro DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    registro DATETIME DEFAULT CURRENT_TIMESTAMP,
     anexo LONGBLOB NOT NULL,
     obs CHAR(255),
-    situacao INT(1) NOT NULL,
+    situacao TINYINT(1) DEFAULT 0,
     usuario_id BIGINT(12) UNSIGNED,
     FOREIGN KEY (usuario_id) REFERENCES usuario(id)
 );
@@ -31,7 +31,7 @@ CREATE TABLE docente (
 );
 
 CREATE TABLE falta (
-    faltas INT(1),
+    faltas TINYINT(1),
     requerimento_id INT,
     docente_id INT,
     FOREIGN KEY (requerimento_id) REFERENCES requerimento(id),
