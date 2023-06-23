4c0e279cf0a6fe66d6bf292a500ac5c8addec3ad
Kaique Nobre <dankmemerson@tutanota.com>
2023-06-23 11:48:22 -0300

    Unsigned a bunch of stuff

--- base.sql
+++ base.sql
@@ -6,22 +6,22 @@ CREATE TABLE usuario (
     nome CHAR(255) NOT NULL,
     email CHAR(255) NOT NULL,
     fone BIGINT(11) UNSIGNED,
-    curso TINYINT(1) NOT NULL,
-    turma TINYINT(1),
+    curso TINYINT(1) UNSIGNED NOT NULL,
+    turma TINYINT(1) UNSIGNED,
     superuser BOOLEAN DEFAULT 0,
     psswd char(255) NOT NULL
 );
 
 CREATE TABLE requerimento (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    objeto TINYINT(1) NOT NULL,
+    objeto TINYINT(1) UNSIGNED NOT NULL,
     inicio DATE NOT NULL,
     termino DATE NOT NULL,
     registro DATETIME DEFAULT CURRENT_TIMESTAMP,
     anexo LONGBLOB NOT NULL,
     obs CHAR(255),
-    situacao TINYINT(1) DEFAULT 0,
-    usuario_id BIGINT(12) UNSIGNED,
+    situacao TINYINT(1) DEFAULT 0 UNSIGNED,
+    usuario_id BIGINT(12) UNSIGNED NOT NULL,
     FOREIGN KEY (usuario_id) REFERENCES usuario(id)
 );
 
@@ -31,7 +31,7 @@ CREATE TABLE docente (
 );
 
 CREATE TABLE falta (
-    faltas TINYINT(1),
+    faltas TINYINT(1) UNSIGNED NOT NULL,
     requerimento_id INT,
     docente_id INT,
     FOREIGN KEY (requerimento_id) REFERENCES requerimento(id),
