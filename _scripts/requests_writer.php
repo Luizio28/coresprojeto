454a5cae0005bba181ee3f83585299994451f0bc
Kaique Nobre <dankmemerson@tutanota.com>
2023-06-25 20:25:52 -0300

    Aesthetics

--- _scripts/requests_writer.php
+++ _scripts/requests_writer.php
@@ -11,7 +11,7 @@ if (isset($_POST['send'])) {
         $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
         $stmt = $pdo->prepare("INSERT INTO requerimento (objeto, inicio, termino, anexo, obs, usuario_id)
-                               VALUES (:objeto, :inicio, :termino, :anexo, :obs, :usuario_id)");
+            VALUES (:objeto, :inicio, :termino, :anexo, :obs, :usuario_id)");
 
         $stmt->bindParam(':objeto', $_POST['objeto']);
         $stmt->bindParam(':inicio', $_POST['inicio']);
