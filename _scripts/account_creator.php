b845d3d1443856ef956599626962ee5eb86c048e
Kaique Nobre <dankmemerson@tutanota.com>
2023-06-25 21:46:13 -0300

    whoops

--- _scripts/account_creator.php
+++ _scripts/account_creator.php
@@ -27,7 +27,7 @@ if (isset($_POST['send'])) {
 
         setcookie('id', $_POST['id'], time() + 3600, "/");
 
-        $directory = $superuser? "administrador" : "usuario";
+        $directory = $is_superuser? "administrador" : "usuario";
 
         header("Location: ../$directory/");
 
