1195519f4bbe256b6ee09067e08cf8fe1774988a
Kaique Nobre <dankmemerson@tutanota.com>
2023-06-26 12:36:12 -0300

    added icon to header

--- _templates/header.php
+++ _templates/header.php
@@ -1,12 +1,17 @@
 <header>
-    <img src="../_img/logo_ifba.webp" alt="Logo do IFBA - campus Eunápolis"  style="height: 6em; aspect-ratio: preserve;">
-    <p>
-        <?php
-        if (isset($_SESSION['id'])) {
-            echo $_SESSION['id'];
-        }else{
-            echo "você ainda não está logado";
-        }
-        ?>
-    </p>
+    <div class="spaced-between flex-row">
+        <img src="../_img/logo_ifba.webp" alt="Logo do IFBA - campus Eunápolis" style="height: 6em; aspect-ratio: preserve;">
+
+        <nav class="flex-row">
+            <?php
+            if (isset($_SESSION['id'])) {
+                echo  "<span class='material-symbols-rounded'>account_circle</span>";
+
+                echo "<p>" . $_SESSION['id'] . "</p>";
+            } else {
+                echo "<p>você ainda não está logado</p>";
+            }
+            ?>
+        </nav>
+    </div>
 </header>
