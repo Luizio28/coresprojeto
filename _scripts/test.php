ac7842bea1aaac8ba6cb18653155def88756aaa8
Kaique Nobre <dankmemerson@tutanota.com>
2023-06-26 08:31:24 -0300

    added session test

--- _scripts/test.php
+++ _scripts/test.php
@@ -1,13 +1,29 @@
 <?php
+session_start();
 
-// Hash the key using SHA-256
- // Use 'true' to return raw binary output
+function session_test()
+{
+    // Check if a counter variable is set in the session
+    if (!isset($_SESSION['counter'])) {
+        // If not set, initialize the counter to 1
+        $_SESSION['counter'] = 1;
+    } else {
+        // If already set, increment the counter
+        $_SESSION['counter']++;
+    }
 
-function encrypt($data,$key){
+    // Display the current value of the counter
+    echo "Counter: " . $_SESSION['counter'];
+}
+
+function encrypt($data, $key)
+{
+    // Hash the key using SHA-256
+    // Use 'true' to return raw binary output
     $hashedKey = hash('sha256', $key, true);
-// Encrypt the data
-$salt = random_bytes(16);
-return openssl_encrypt($data, 'aes-256-cbc', $hashedKey, OPENSSL_RAW_DATA, $salt);
+    // Encrypt the data
+    $salt = random_bytes(16);
+    return openssl_encrypt($data, 'aes-256-cbc', $hashedKey, OPENSSL_RAW_DATA, $salt);
 }
 
 // Decrypt the data
