d9482f58b3b9092ca60367fed77c8dfeee94c774
Kaique Nobre <dankmemerson@tutanota.com>
2023-06-26 12:34:31 -0300

    Added material icon pkg

--- _templates/head.php
+++ _templates/head.php
@@ -7,10 +7,12 @@ function head_constructor($title, $su = false)
     $is_sign_in = $title == "sign-in" | $title == "sign-up";
 
     include "../_scripts/session_validator.php";
-
+    
     echo "
     <meta charset='UTF-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+
+    <link href='../node_modules/material-symbols/index.css' rel='stylesheet'>
 
     <link rel='stylesheet' href='../style.css'>
     <link rel='icon' href='../_img/ico.webp'>
