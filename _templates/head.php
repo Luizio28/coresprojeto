85f26eb1e2782e88ffe5d167c31f8a76fa21832b
Kaique Nobre <dankmemerson@tutanota.com>
2023-06-27 11:58:01 -0300

    weird test

--- _templates/head.php
+++ _templates/head.php
@@ -6,8 +6,23 @@ function head_constructor($title, $su = false)
     $superuser_only = $su;
     $is_sign_in = $title == "sign-in" | $title == "sign-up";
 
-    include "../_scripts/session_validator.php";
-    
+    $valid_session_id = isset($_SESSION['id']);
+
+    if ($superuser_only) {
+        $is_superuser = $_SESSION['superuser'] == true;
+        $valid_session = $is_superuser & $valid_session_id;
+    } else {
+        $valid_session = $valid_session_id;
+    }
+
+    if ($valid_session_id & !$valid_session | $is_sign_in & $valid_session_id) {
+        header("Location: ../" . $_SESSION['directory'] . "/");
+        exit;
+    } else if (!$valid_session_id & !$valid_session & !$is_sign_in) {
+        header("Location: ../sign-in/");
+        exit;
+    }
+
     echo "
     <meta charset='UTF-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
