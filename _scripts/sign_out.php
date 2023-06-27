5950fa5f7da1345ad4b77eedf939557a46ed32ec
Kaique Nobre <dankmemerson@tutanota.com>
2023-06-27 14:19:02 -0300

    Made sign-out work

--- _scripts/sign_out.php
+++ _scripts/sign_out.php
@@ -1,4 +1,20 @@
 <?php
+//Fonte https://stackoverflow.com/questions/3512507/proper-way-to-logout-from-a-session-in-php
+
+// Unset all of the session variables.
 $_SESSION = array();
 
+// If it's desired to kill the session, also delete the session cookie.
+// Note: This will destroy the session, and not just the session data!
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+// Finally, destroy the session.
 session_destroy();
+header("Location: ../sign-in/");
+exit;
