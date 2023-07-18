6c66b04ce6ff15e34cc374a38e3710bc9428cd8b
Luizio28 <79097547+Luizio28@users.noreply.github.com>
2023-07-18 08:10:22 -0300

    Made so the PDFs are saved to a directory instead of the db

--- _scripts/requests_writer.php
+++ _scripts/requests_writer.php
@@ -10,17 +10,24 @@ if (isset($_POST['send'])) {
 
         $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
-        $statement = $pdo->prepare("INSERT INTO requerimento (objeto, inicio, termino, anexo, obs, usuario_id)
-            VALUES (:objeto, :inicio, :termino, :anexo, :obs, :usuario_id)");
+        $statement = $pdo->prepare("INSERT INTO requerimento (objeto, inicio, termino, obs, usuario_id)
+            VALUES (:objeto, :inicio, :termino, :obs, :usuario_id)");
 
         $statement->bindParam(':objeto', $_POST['objeto']);
         $statement->bindParam(':inicio', $_POST['inicio']);
         $statement->bindParam(':termino', $_POST['termino']);
-        $statement->bindParam(':anexo', $_POST['anexo']);
         $statement->bindParam(':obs', $_POST['obs']);
         $statement->bindParam(':usuario_id', $_SESSION['id']);
 
         $statement->execute();
+            
+        $statement = $pdo->prepare("SELECT LAST(*) FROM requerimento WHERE usuario_id = ".$_SESSION['id']."");
+        $statement->execute();
+        $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+        
+        $dir = anexo/$_SESSION['id']."_".$result[0]["id"]."_".$result[0]["registro"].".pdf";
+            
+        move_uploaded_file($_FILES['anexo'], $dir);
     } catch (PDOException $exception) {
         handle_pdo_exception($exception);
     }
