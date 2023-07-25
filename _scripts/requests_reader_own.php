bfdf4cd3d1b533091868cf2b7453517038fb786b
Kaique Nobre <dankmemerson@tutanota.com>
2023-07-25 20:43:12 -0300

    segregated the javascript and properly linked the pdf files

--- _scripts/requests_reader_own.php
+++ _scripts/requests_reader_own.php
@@ -65,7 +65,7 @@
                 <td>" . $row['inicio'] . "</td>
                 <td>" . $row['termino'] . "</td>
                 <td class='break-text'>" . $row['obs'] . "</td>
-                <td class='break-text'><a href=" . $row['anexo'] . ">link</a></td>
+                <td class='break-text'><a href= ../anexo/" . $row['diretorio_anexo'] . "  target='_blank'>link</a></td>
                 <td>" . $situation . "</td>
             </tr>
             ";
@@ -79,61 +79,3 @@
 
     ?>
 </table>
-
-<script>
-    //Código descaradamente """"inpirado"""" nesse aqui ó
-    //https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_sort_table_desc
-    function sortTable(n) {
-        var rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-        switching = true;
-        //Set the sorting direction to ascending:
-        dir = 'asc';
-        /*Make a loop that will continue until
-        no switching has been done:*/
-        while (switching) {
-            //start by saying: no switching is done:
-            switching = false;
-            rows = table.rows;
-            /*Loop through all table rows (except the
-            first, which contains table headers):*/
-            for (i = 1; i < (rows.length - 1); i++) {
-                //start by saying there should be no switching:
-                shouldSwitch = false;
-                /*Get the two elements you want to compare,
-                one from current row and one from the next:*/
-                x = rows[i].getElementsByTagName('TD')[n];
-                y = rows[i + 1].getElementsByTagName('TD')[n];
-                /*check if the two rows should switch place,
-                based on the direction, asc or desc:*/
-                if (dir == 'asc') {
-                    if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-                        //if so, mark as a switch and break the loop:
-                        shouldSwitch = true;
-                        break;
-                    }
-                } else if (dir == 'desc') {
-                    if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-                        //if so, mark as a switch and break the loop:
-                        shouldSwitch = true;
-                        break;
-                    }
-                }
-            }
-            if (shouldSwitch) {
-                /*If a switch has been marked, make the switch
-                and mark that a switch has been done:*/
-                rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-                switching = true;
-                //Each time a switch is done, increase this count by 1:
-                switchcount++;
-            } else {
-                /*If no switching has been done AND the direction is 'asc',
-                set the direction to 'desc' and run the while loop again.*/
-                if (switchcount == 0 && dir == 'asc') {
-                    dir = 'desc';
-                    switching = true;
-                }
-            }
-        }
-    }
-</script>
