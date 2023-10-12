2ea74b53e41640ca4a9780d69aa4ffacfafb89ca
Zer0-Zer0 <dankmemerson@tutanota.com>
2023-10-12 19:14:23 -0300

    .

--- _templates/head.php
+++ _templates/head.php
@@ -16,8 +16,6 @@ function head_constructor($title, $su = false)
     <meta charset='UTF-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
 
-    <link href='../node_modules/material-symbols/index.css' rel='stylesheet'>
-
     <link rel='stylesheet' href='../style.css'>
     <link rel='icon' href='../_img/ico.webp'>
 
