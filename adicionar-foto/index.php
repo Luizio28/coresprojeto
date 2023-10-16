0da82b3e6a74ad51afb91a445dd925b9c2373074
Gustavo <136453175+GuPeixe@users.noreply.github.com>
2023-10-16 17:19:12 -0300

    Função de adicionar fotos, a ser revisado

---
+++ adicionar-foto/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <?php
+    include "../_templates/echoer.php";
+    include "../_templates/head.php";
+
+    head_constructor("Novo requerimento");
+    ?>
+
+    <meta name="description" id="description" content="Essa é a página em que requerimentos são escritos e enviados pelos usuários para CORES do IFBA Campus Eunápolis">
+</head>
+
+<body>
+    <?php
+    include "../_templates/header.php";
+    ?>
+
+    <main>
+        <div class="flex-column">
+            <h1>Novo requerimento</h1>
+
+            <form enctype="multipart/form-data" id="create-new" class="center-flex flex-column box" method="post">
+                <!-- ... outras partes do formulário ... -->
+
+                <div class="spaced-between flex-column">
+                    <label>
+                        Foto de Perfil
+                    </label>
+                    <input type="file" name="foto_perfil" id="foto_perfil" accept="image/*">
+                    <span class="tooltip" id="tp1">Adicione uma foto de perfil (imagem) opcional.</span>
+                </div>
+
+                <div>
+                    <input type="submit" name="send" id="send" value="criar">
+                    <input type="reset" value="reset">
+                    <a href="../usuario">voltar</a>
+                </div>
+            </form>
+
+            <?php
+            include "../_scripts/requests_writer.php";
+            ?>
+        </div>
+    </main>
+
+    <?php
+    show("../_templates/footer.html");
+    ?>
+</body>
+
+</html>
